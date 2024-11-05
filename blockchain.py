3e8aa1489eec11857c289fba110a4d6b2bfbd32f
EMMMABK <adilbikiev2@gmail.com>
2024-11-05 14:30:10 +0600

    add 10th commit

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,15 @@
 from hashlib import sha256
 
 def updatehash(*args):
-    hashing_text = ""; h=sha256()
+    hashing_text = ""
+    h = sha256()
     for arg in args:
         hashing_text += str(arg)
 
     h.update(hashing_text.encode('utf-8'))
     return h.hexdigest()
 
-class Block():
+class Block:
     data = None
     hash = None
     nonce = 0
@@ -20,46 +21,45 @@ class Block():
 
     def hash(self):
         return updatehash(
-            self.previous_hash, 
-            self.number, 
-            self.data, 
+            self.previous_hash,
+            self.number,
+            self.data,
             self.nonce
         )
 
     def __str__(self):
-        return str("Block: %s\nHash: %s\nPrevious:" +
-                   "%s\nData: %s\nNonce: %s\n" %(
-                       self.number,self.hash(),
-                       self.previous_hash,
-                       self.data,
-                       self.nonce
-                       )
-                    )
+        return (
+            "Block: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" % (
+                self.number,
+                self.hash(),
+                self.previous_hash,
+                self.data,
+                self.nonce
+            )
+        )
 
-class Blockchain():
+class Blockchain:
     difficulty = 5
 
-    def __init__(self,chain=[]):
+    def __init__(self, chain=[]):
         self.chain = chain
-    
+
     def add(self, block):
-        self.chain.append({
-            'hash': block.hash(), 
-            'previous': block.previous_hash, 
-            'number': block.number, 
-            'data': block.data, 
-            'nonce': block.nonce
-        })
-    
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
+
     def mine(self, block):
         try:
-            block.previous_hash = self.chain[-1].get('hash')
+            block.previous_hash = self.chain[-1].hash()
         except IndexError:
             pass
 
         while True:
             if block.hash()[:self.difficulty] == "0" * self.difficulty:
-                self.add(block); break
+                self.add(block)
+                break
             else:
                 block.nonce += 1
 
@@ -70,7 +70,7 @@ def main():
     num = 0
     for data in database:
         num += 1
-        blockchain.mine(Block(data,num))
+        blockchain.mine(Block(data, num))
 
     for block in blockchain.chain:
         print(block)
